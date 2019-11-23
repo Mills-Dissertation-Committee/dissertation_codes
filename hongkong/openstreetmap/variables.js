f2b79005709e49539eb86205f601cc881d641367
davidamills <dam203@txstate.edu>
2019-11-23 11:11:41 -0600

    Variable edit

--- hongkong/openstreetmap/variables.js
+++ hongkong/openstreetmap/variables.js
@@ -1,4 +1,17 @@
-tear_gas = d3.csv("tear_gas_data.csv")
+var field1=[];
+var field2=[];
+
+d3.csv("data.csv",function(csv){
+            csv.map(function(d){
+                field1.push(d.name);
+                field2.push(+d.value);
+            })
+            console.log("field1",field1);
+console.log("field2",field2);
+console.log("field1",field1[0]);
+        });
+
+
 
 function stationInfo(StationCode, latitude, longitude, Windlatitude, Windlongitude, StationName, WebcamAngle) {
   this.StationCode = StationCode;
