92aa55d70a0519227aef9b53996ee21e49e8b9fe
davidamills <dam203@txstate.edu>
2019-11-23 12:31:49 -0600

    Hid weather markers

--- hongkong/openstreetmap/map.js
+++ hongkong/openstreetmap/map.js
@@ -7,8 +7,10 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
   id: 'mapbox.streets'
 }).addTo(mymap);
 
+/*
 for(i = 0; i < AWS_Station.length; i++)
 {
   L.marker([parseFloat(AWS_Station[i].latitude),parseFloat(AWS_Station[i].longitude)]).addTo(mymap)
   .bindPopup("<b>" + AWS_Station[i].StationName.name_UC + "</b><br />Weather Data Here.");
 }
+*/
