c56f2237f701673ce7fe2f0bb4937d5881700141
davidamills <dam203@txstate.edu>
2020-01-31 18:57:04 -0600

    Improved the scalability of icons.

--- js/page_engine.js
+++ js/page_engine.js
@@ -1,25 +1,72 @@
 // Boolean variable that keeps track of current page layout.
 var page_layout = false;
 
-// The center circle and four icon circles.
-var circle_div = document.getElementById('circle_center');
-var circle_one = document.getElementById('circle_one');
-var circle_two = document.getElementById('circle_two');
-var circle_three = document.getElementById('circle_three');
-var circle_four = document.getElementById('circle_four');
-
 // The number of divs above in the icon class.
-var icons = 4;
+var icons = 10;
+
+document.write("<style>");
+document.write("#circle_center { background: url('./img/2x/me@2x.png'); background-size: 100%; }");
+for(i = 0; i < icons; i++)
+{
+  document.write("#circle_" + i +
+  "{" +
+  	"background: url('./img/1x/clipboard.png');" +
+  	"background-size: 100%;" +
+  	"margin-top: 50vh;" +
+  	"margin-left: 50vw;" +
+  "}" +
+  "#circle_" + i + ":hover" +
+  "{" +
+    "background: url('./img/1x/clipboard_hover.png');" +
+  	"background-size: 100%;" +
+  "}"
+  );
+}
+document.write("</style>");
+
+// Create the initial CSS for the icons.
+document.write("<div id='circle_center'></div>");
+var circle_div = document.getElementById('circle_center');
+
+for(i = 0; i < icons; i++)
+{
+  document.write("<div id='circle_" + i + "' class='icon'></div>");
+}
 
 // Stores the height and width of the page.
 var height;
 var width;
 
-// Array for placing icons.
+// Arrays for placing icons.
 var icon_rotate = [];
+var icon_list = [];
 for (i = 0; i < icons; i++)
 {
+  icon_list[i] = 'circle_' + i;
   icon_rotate[i] = (360 - (i * (360 / icons)) - ((360 / icons) / 2));
+}
+var icon_left_horizontal = [];
+var icon_top_vertical = [];
+var icon_spacing = 1 / (icons + 2);
+var icon_temp = icon_spacing;
+for (i = 0; i < icons; i++)
+{
+  if (i == Math.round(icons / 2))
+  {
+    icon_temp = icon_temp + icon_spacing;
+  }
+  icon_left_horizontal[i] = icon_temp;
+  icon_temp = icon_temp + icon_spacing;
+}
+icon_temp = icon_spacing;
+for (i = 0; i < icons; i++)
+{
+  if (i == Math.round(icons / 2))
+  {
+    icon_temp = icon_temp + icon_spacing;
+  }
+  icon_top_vertical[i] = icon_temp;
+  icon_temp = icon_temp + icon_spacing;
 }
 
 var circle_top = .5;
@@ -29,11 +76,8 @@ var circle_left_horizontal = .5;
 var circle_top_vertical = .5;
 var circle_left_vertical = .55;
 var circle = 'circle_center';
-var icon_top_horizontal = [.65, .65, .65, .65];
-var icon_left_horizontal = [.1, .3, .7, .9];
-var icon_top_vertical = [.125, .325, .7, .9];
-var icon_left_vertical = [.55, .55, .55, .55];
-var icon_list = ['circle_one','circle_two','circle_three','circle_four'];
+var icon_top_horizontal = .65;
+var icon_left_vertical = .55;
 
 var circle_diameter;
 var circle_radius;
@@ -68,7 +112,7 @@ function set_location()
     $(eval(circle)).css('margin-left', margin_left + 'px');
     for (i=0;i < icons;i++)
     {
-      var margin_top = icon_top_horizontal[i] * height - circle_radius + 5;
+      var margin_top = icon_top_horizontal * height - circle_radius + 5;
       var margin_left = icon_left_horizontal[i] * width - circle_radius;
       $(eval(icon_list[i])).css('margin-top', margin_top + 'px');
       $(eval(icon_list[i])).css('margin-left', margin_left + 'px');
@@ -82,7 +126,7 @@ function set_location()
     for (i=0;i < icons;i++)
     {
       var margin_top = icon_top_vertical[i] * height - circle_radius;
-      var margin_left = icon_left_vertical[i] * width - circle_radius;
+      var margin_left = icon_left_vertical * width - circle_radius;
       $(eval(icon_list[i])).css('margin-top', margin_top + 'px');
       $(eval(icon_list[i])).css('margin-left', margin_left + 'px');
     }
@@ -102,7 +146,7 @@ function set_location()
   }
 }
 
-function fit_center_circle(div,div_one,div_two,div_three,div_four) {
+function fit_center_circle() {
   // Gets height and width of window.
   height = $(window).height();
   width = $(window).width();
@@ -137,9 +181,9 @@ function fit_center_circle(div,div_one,div_two,div_three,div_four) {
 }
 
 // Sets the initial size and location for all elements.
-fit_center_circle(circle_div,circle_one,circle_two,circle_three,circle_four);
+fit_center_circle();
 
 // Function checks for window resize and adjusts accordingly.
 $(window).resize(function () {
-  fit_center_circle(circle_div,circle_one,circle_two,circle_three,circle_four);
+  fit_center_circle();
 });
