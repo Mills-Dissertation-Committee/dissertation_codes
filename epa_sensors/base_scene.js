9524d13dee9aad228317ce062a401c84c612d0a1
davidamills <dam203@txstate.edu>
2022-04-22 12:17:33 -0500

    Reset to base scene.

--- epa_sensors/base_scene.js
+++ epa_sensors/base_scene.js
@@ -1,9 +1,7 @@
-require(["esri/Map", "esri/views/SceneView", "esri/layers/Layer", "esri/layers/GraphicsLayer", "esri/Graphic"], (
+require(["esri/Map", "esri/views/SceneView", "esri/layers/Layer"], (
   Map,
   SceneView,
-  Layer,
-  GraphicsLayer,
-  Graphic
+  Layer
 ) => {
   const map = new Map({
     basemap: "gray-vector"
@@ -15,47 +13,5 @@ require(["esri/Map", "esri/views/SceneView", "esri/layers/Layer", "esri/layers/G
     zoom: 7,
     center: [-87, 34]
   });
-
-  /************************************************
-   *
-   * Create a layer from a GEOjson layer.
-   *
-   *
-   *************************************************/
-
-   const counties_renderer = {
-     type: "simple-fill",
-     color: [227, 139, 79, 0.8], // Orange, opacity 80%
-     outline: {
-       color: [255, 255, 255],
-       width: 1
-     }
-   };
-
-   require(["esri/layers/GeoJSONLayer"], function(GeoJSONLayer){
-   // points to the states layer in a service storing U.S. census data
-   const geojsonlayer = new GeoJSONLayer({
-     url: "geojson_files/region_11_counties.geojson",
-   });
-
-   const fillSymbol = {
-      type: "simple-fill", // autocasts as new SimpleFillSymbol()
-      color: [227, 139, 79, 0.8],
-      outline: {
-        // autocasts as new SimpleLineSymbol()
-        color: [255, 255, 255],
-        width: 2
-      }
-    };
-
-    const polygonGraphic = new Graphic({
-      geometry: geojsonlayer,
-      symbol: fillSymbol
-    });
-
-    map.add(polygonGraphic);
-
-   //map.add(geojsonlayer);  // adds the layer to the map
- });
 
 });
