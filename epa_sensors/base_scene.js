1ffe838bab75dc1b6f446e54bdb153405bdc87df
davidamills <dam203@txstate.edu>
2022-04-27 07:44:17 -0500

    Adding counties.

--- epa_sensors/base_scene.js
+++ epa_sensors/base_scene.js
@@ -14,4 +14,13 @@ require(["esri/Map", "esri/views/SceneView", "esri/layers/Layer"], (
     center: [-87, 34]
   });
 
+  require(["esri/layers/GeoJSONLayer"], function(GeoJSONLayer){
+    // points to the states layer in a service storing U.S. census data
+    const geojsonlayer = new GeoJSONLayer({
+      url: "geojson_files/region_11_counties.json"
+    });
+    
+    map.add(geojsonlayer);  // adds the layer to the map
+  });
+
 });
