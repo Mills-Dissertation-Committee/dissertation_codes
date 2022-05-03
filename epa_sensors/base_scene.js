9753b55a0658b74aab6f11d59a32de8afd78fc81
davidamills <dam203@txstate.edu>
2022-05-03 18:28:20 -0500

    Changed geojsonlayer to defined featurelayer const.

--- epa_sensors/base_scene.js
+++ epa_sensors/base_scene.js
@@ -136,7 +136,7 @@ require(["esri/Map",
             view: view,
             layerInfos: [
               {
-                layer: geojsonlayer,
+                layer: featurelayer,
                 title: "Region 11 Counties"
               }
             ]
