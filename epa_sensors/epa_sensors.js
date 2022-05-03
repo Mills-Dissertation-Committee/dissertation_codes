10771efd156b96acd992dfb4c4f3163e3685aaa4
davidamills <dam203@txstate.edu>
2022-05-02 19:42:07 -0500

    Fixed unexpected end of input.

--- epa_sensors/epa_sensors.js
+++ epa_sensors/epa_sensors.js
@@ -55,4 +55,5 @@ require([
 
           map.add(epaLayer);
         }
+      });
 });
--- epa_sensors/loadScripts.js
+++ epa_sensors/loadScripts.js
@@ -3,14 +3,14 @@ function loadScript(src, callback)
         let script = document.createElement('script')  // Creates the <script> element.
         document.head.append(script)                   // Places the element in the document's <head> section.
         script.src = src                               // Adds the src, <script src='...'>.
-        script.onload = () => callback()               // Adds the onload, <script src='...' onload='...()'>
+        script.onload = () => callback()               // Adds the onload, <script src='...' onload='...()'>.
   }
 
 loadScript('https://js.arcgis.com/4.23/', function() {      // Esri 4.23 API.
   loadScript('variables.js', function() {                   // Variables.
     loadScript('base_scene.js', function() {                // Loads base scene and requirements.
       loadScript('epa_sensors.js', function() {             // Loads EPA sensors.
-        
+
       })
     })
   })
